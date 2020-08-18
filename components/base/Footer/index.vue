4d6b98a32052b8c0bef8fbcebd642fb4c301512d
Samuel Leonel <samueleonelfreitas@hotmail.com>
2020-08-18 15:07:15 -0300

    Adição do footer

---
+++ components/base/Footer/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <footer class="footer">
+    <Logo class="logo"/>
+    <p class="frase">Viver, aprender e evoluir um pouco mais a cada dia.</p>
+    <ul class="social">
+      <li class="item">
+        <a class="link" href="https://github.com/samuel-lf" target="blank">
+          <GithubLogo />
+        </a>
+      </li>
+      <li class="item">
+        <a class="link" href="https://www.linkedin.com/in/samuel-leonel-4a9ab713" target="blank">
+          <LinkedInLogo />
+        </a>
+      </li>
+      <li class="item">
+        <a class="link" href="https://www.facebook.com/samuellf97" target="blank">
+          <FacebookLogo />
+        </a>
+      </li>
+      <li class="item">
+        <a class="link" href="https://www.instagram.com/samuel_lf/" target="blank">
+          <InstagramLogo />
+        </a>
+      </li>
+      <li class="item">
+        <a class="link" href="https://twitter.com/Samuel__Leonel" target="blank">
+          <TwitterLogo />
+        </a>
+      </li>
+    </ul>
+    <p class="feito">
+      Feito com <Heart class="heart"/>
+    </p>
+  </footer>
+</template>
+
+<script>
+import FacebookLogo from '~/assets/images/social/facebook.svg?inline'
+import LinkedInLogo from '~/assets/images/social/linkedin.svg?inline'
+import GithubLogo from '~/assets/images/social/github.svg?inline'
+import InstagramLogo from '~/assets/images/social/instagram.svg?inline'
+import TwitterLogo from '~/assets/images/social/twitter.svg?inline'
+import Heart from '~/assets/images/social/heart.svg?inline'
+import Logo from '~/assets/images/logo/logo.svg?inline'
+
+
+export default {
+  name: 'Footer',
+  components:{
+    FacebookLogo,
+    LinkedInLogo,
+    GithubLogo,
+    InstagramLogo,
+    TwitterLogo,
+    Heart,
+    Logo
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import './styles';
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -4,14 +4,17 @@
     <main>
       <Nuxt />
     </main>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from "../components/base/Header";
+import Footer from "../components/base/Footer";
 export default {
   components: {
-    Header
+    Header,
+    Footer
   }
 }
 </script>
