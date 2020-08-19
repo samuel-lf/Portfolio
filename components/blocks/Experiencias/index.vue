d9b48e1f2bf9115a0e74f04293b54d53a611fd71
Samuel Leonel <samueleonelfreitas@hotmail.com>
2020-08-19 15:58:36 -0300

    Adição da seção de experiências

---
+++ components/blocks/Experiencias/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <section id="experiencias" class="section">
+    <h2 class="title">Experiências</h2>
+    <p class="subtitle">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo tempora dignissimos inventore debitis repellendus sequi consectetur eligendi quibusdam. Fuga itaque optio id deleniti, distinctio ratione fugit voluptate vel tempora veniam?</p>
+    <div class="timeline">
+      <div class="record">
+        <div class="date">
+            <div>Outubro 2019 - Atual</div>
+        </div>
+        <div class="memory">
+            <h2>Seguralta Corretora de Seguros</h2>
+            <p><span>Cargo: </span>Desenvolvedor Front-End</p>
+            <p><span>Atuação: </span>Atuar no desenvolvimento de sistemas internos da empresa.</p>
+        </div>
+      </div>
+      <div class="record">
+        <div class="date">
+            <div>Setembro 2018 - Outubro 2019</div>
+        </div>
+        <div class="memory">
+            <h2>Guardião Digital</h2>
+            <p><span>Cargo: </span>Desenvolvedor FullStack</p>
+            <p><span>Atuação: </span>Atuar no desenvolvimento de sistemas internos e externos da empresa</p>
+        </div>
+      </div>
+      <div class="record">
+        <div class="date">
+            <div>Abril 2018 - Agosto 2018</div>
+        </div>
+        <div class="memory">
+            <h2>Field Control</h2>
+            <p><span>Cargo: </span>Estagiário (Desenvolvedor Front-End)</p>
+            <p><span>Atuação: </span>Auxiliar no desenvolvimento de soluções e atualizações do sistema próprio da empresa</p>
+        </div>
+      </div>
+      <div class="record">
+        <div class="date">
+            <div>Janeiro 2017 - Março 2018</div>
+        </div>
+        <div class="memory">
+            <h2>Colégio Arte Manha e Coeso</h2>
+            <p><span>Cargo: </span>Estagiário</p>
+            <p><span>Atuação: </span>Auxiliar na organização e nas tarefas do setor de T.I da escola</p>
+        </div>
+      </div>
+      <div class="record">
+        <div class="date">
+            <div>Outubro 2015 - Novembro 2015</div>
+        </div>
+        <div class="memory">
+            <h2>Super Muffato</h2>
+            <p><span>Cargo: </span>Empacotador</p>
+            <p><span>Atuação: </span>Auxiliar no empacotamento das compras do cliente e realizar o atendimento do mesmo</p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+@import './styles';
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,20 @@
   <div>
     <Principal />
     <Habilidades />
+    <Experiencias />
   </div>
 </template>
 
 <script>
 import Principal from '../components/blocks/Principal'
 import Habilidades from '../components/blocks/Habilidades'
+import Experiencias from '../components/blocks/Experiencias'
 
 export default {
   components: {
     Principal,
-    Habilidades
+    Habilidades,
+    Experiencias
   }
 }
 </script>
