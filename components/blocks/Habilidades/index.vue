e8b3a326be1de9fdae417abef2e91a6eadd8a6a2
Samuel Leonel <samueleonelfreitas@hotmail.com>
2020-08-19 10:16:13 -0300

    Adição da seção de habilidades

---
+++ components/blocks/Habilidades/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <section id="habilidades" class="section">
+    <h2 class="title">Habilidades</h2>
+    <p class="subtitle">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo tempora dignissimos inventore debitis repellendus sequi consectetur eligendi quibusdam. Fuga itaque optio id deleniti, distinctio ratione fugit voluptate vel tempora veniam?</p>
+    <div class="container">
+      <div class="card">
+        <img class="image" src="~/assets/images/technology/html5.svg" alt="Logo do HTML 5">
+        <p class="technology">HTML 5</p>
+      </div>
+      <div class="card">
+        <img class="image" src="~/assets/images/technology/css3.svg" alt="Logo do CSS 3">
+        <p class="technology">CSS 3</p>
+      </div>
+      <div class="card">
+        <img class="image" src="~/assets/images/technology/javascript.svg" alt="Logo do Javascript">
+        <p class="technology">Javascript</p>
+      </div>
+      <div class="card">
+        <img class="image" src="~/assets/images/technology/angular.svg" alt="Logo do Angular">
+        <p class="technology">Angular 2+</p>
+      </div>
+      <div class="card">
+        <img class="image" src="~/assets/images/technology/react.svg" alt="Logo do React">
+        <p class="technology">ReactJS/GatsbyJS</p>
+      </div>
+      <div class="card">
+        <img class="image" src="~/assets/images/technology/vue_js.svg" alt="Logo do VueJS">
+        <p class="technology">VueJS/NuxtJS</p>
+      </div>
+      <div class="card">
+        <img class="image" src="~/assets/images/technology/flutter.svg" alt="Logo do Flutter">
+        <p class="technology">Flutter</p>
+      </div>
+      <div class="card">
+        <img class="image" src="~/assets/images/technology/adobexd.svg" alt="Logo do Adobe XD">
+        <p class="technology">Adobe XD</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import './styles.scss';
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
     <Principal />
+    <Habilidades />
   </div>
 </template>
 
 <script>
 import Principal from '../components/blocks/Principal'
+import Habilidades from '../components/blocks/Habilidades'
+
 export default {
   components: {
-    Principal
+    Principal,
+    Habilidades
   }
 }
 </script>
