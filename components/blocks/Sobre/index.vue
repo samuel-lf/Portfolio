e496c9674ce655388a0097b93e055a9a844186f5
Samuel Leonel <samueleonelfreitas@hotmail.com>
2020-08-20 08:55:10 -0300

    Ajustes no sobre mim

--- components/blocks/Sobre/index.vue
+++ components/blocks/Sobre/index.vue
@@ -1,7 +1,6 @@
 <template>
   <section id="sobre" class="section">
     <h2 class="title">Sobre mim</h2>
-    <p class="subtitle">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo tempora dignissimos inventore debitis repellendus sequi consectetur eligendi quibusdam. Fuga itaque optio id deleniti, distinctio ratione fugit voluptate vel tempora veniam?</p>
     <div class="sobreMim">
       <p class="text">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci porro ratione neque qui omnis possimus mollitia amet, corporis libero quo esse, tempora quae ea similique! At deleniti tenetur hic nulla.
