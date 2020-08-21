7668d2cf0c193d13f834b89c36939999c3f6ac3b
Samuel Leonel <samueleonelfreitas@hotmail.com>
2020-08-20 22:27:43 -0300

    Adição dos textos da seção sobre mim

--- components/blocks/Sobre/index.vue
+++ components/blocks/Sobre/index.vue
@@ -2,12 +2,14 @@
   <section id="sobre" class="section">
     <h2 class="title">Sobre mim</h2>
     <div class="sobreMim">
-      <p class="text">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci porro ratione neque qui omnis possimus mollitia amet, corporis libero quo esse, tempora quae ea similique! At deleniti tenetur hic nulla.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci porro ratione neque qui omnis possimus mollitia amet, corporis libero quo esse, tempora quae ea similique! At deleniti tenetur hic nulla.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci porro ratione neque qui omnis possimus mollitia amet, corporis libero quo esse, tempora quae ea similique! At deleniti tenetur hic nulla.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci porro ratione neque qui omnis possimus mollitia amet, corporis libero quo esse, tempora quae ea similique! At deleniti tenetur hic nulla.
-      </p>
+      <div>
+        <p class="hello">Samuel Leonel, ao seu dispor!</p>
+        <p class="text">
+          Formado em Análise e Desenvolvimento de Sistemas pela FATEC e atuando na área de desenvolvimento web há aproximadamente 2 anos e meio.
+          Sou apaixado pela minha profissão e busco sempre aperfeiçoar meus conhecimentos, além de sempre estar buscando novos desafios. Meu hobby favorito é consumir conteúdo sobre tecnologia,
+          desde novas ferramentas que possam auxiliar no desenvolvimento e evolução do meu trabalho até informações sobre aquele novo smartphone que vai sair no final do ano.
+        </p>
+      </div>
       <img class="image" src="~assets/images/samuel_leonel.png" alt="Minha foto">
     </div>
   </section>
