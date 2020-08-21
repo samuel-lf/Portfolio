6a1f463c7ffec50cc46c9a7d0912e4e2741ac2b3
Samuel Leonel <samueleonelfreitas@hotmail.com>
2020-08-20 23:28:44 -0300

    Adição dos textos na seção de contato e envio de email

--- components/blocks/Contato/index.vue
+++ components/blocks/Contato/index.vue
@@ -1,28 +1,28 @@
 <template>
   <section id="contato" class="section sectionSpace">
     <h2 class="title">Vamos conversar?</h2>
-    <p class="subtitle">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo tempora dignissimos inventore debitis repellendus sequi consectetur eligendi quibusdam. Fuga itaque optio id deleniti, distinctio ratione fugit voluptate vel tempora veniam?</p>
-    <form class="form">
+    <p class="subtitle">Podemos tomar um café (mesmo que seja virtual) e prozear sobre como posso auxiliar você desenvolvendo uma solução única.</p>
+    <form class="form" @submit="sendEmail" v-if="emailSend === false">
       <div class="block">
-        <input class="input" type="text" name="name" required>
+        <input class="input" type="text" name="name" v-model="data.nome" required>
         <label for="name" class="label">
           <span class="content">Nome completo</span>
         </label>
       </div>
       <div class="block">
-        <input class="input" type="email" name="email" required>
+        <input class="input" type="email" name="email" v-model="data.email" required>
         <label for="email" class="label">
           <span class="content">E-mail</span>
         </label>
       </div>
       <div class="block">
-        <input class="input" type="text" name="assunto" required>
+        <input class="input" type="text" name="assunto" v-model="data.assunto" required>
         <label for="assunto" class="label">
           <span class="content">Assunto</span>
         </label>
       </div>
       <div class="block">
-        <input class="input" type="text" name="mensagem" required>
+        <input class="input" type="text" name="mensagem" v-model="data.mensagem" required>
         <label for="mensagem" class="label">
           <span class="content">Mensagem</span>
         </label>
@@ -31,11 +31,50 @@
         <button type="submit" class="btn">Enviar</button>
       </div>
     </form>
+    <div class="sendSuccess" v-if="emailSend === true">
+      <SendEmail class="icon"/>
+      <h2 class="title">Sucesso ao enviar sua mensagem</h2>
+      <p class="text">Em breve retornarei o seu contato!</p>
+    </div>
   </section>
 </template>
 
 <script>
-export default {}
+import SendEmail from '~/assets/images/icons/sendEmail.svg?inline'
+
+export default {
+  components: {
+    SendEmail
+  },
+  data(){
+    return {
+      data: {
+        nome: '',
+        email: '',
+        assunto: '',
+        mensagem: ''
+      },
+      emailSend: false
+    }
+  },
+  methods: {
+    sendEmail(event){
+      event.preventDefault();
+      this.$axios({
+        method: 'POST',
+        url: 'https://formspree.io/myynapoj',
+        responseType: 'stream',
+        data: this.data
+      })
+      .then((response) => {
+        console.log(response)
+        if(response.data.ok){
+          this.emailSend = true
+        }
+      });
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
